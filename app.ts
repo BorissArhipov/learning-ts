dd71de72fcd64b5cbd8221929e58e21bddb38c3a
Boriss Arhipov <bori.7166455@gmail.com>
2020-04-29 12:45:23 +0300

    Filtering active and finished projects

--- app.ts
+++ app.ts
@@ -184,7 +184,13 @@ class ProjectList {
         this.element.id = `${type}-projects`;
 
         projectState.addListener((projects: Project[]) => {
-            this.assignedProjects = projects;
+            const relevantProjects = projects.filter(prj => {
+                if(this.type === 'active') {
+                   return prj.status === ProjectStatus.Active;
+                }
+                return prj.status === ProjectStatus.Finished;
+            })
+            this.assignedProjects = relevantProjects;
             this.renderProjects();
         })
 
@@ -194,10 +200,11 @@ class ProjectList {
 
     private renderProjects() {
         const listEl = document.getElementById(`${this.type}-projects-list`)! as HTMLUListElement;
+        listEl.innerHTML = '';
         for(const prjItem of this.assignedProjects) {
             const listItem = document.createElement('li');
             listItem.textContent = prjItem.title;
-            listEl.appendChild(listItem)
+            listEl.appendChild(listItem);
         }
     }
 
